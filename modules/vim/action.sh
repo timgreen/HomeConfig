2aa46656aeb02b1c670fa949262f80e0aaa4b578
Tim Green <iamtimgreen@gmail.com>
2013-12-03 09:37:43 +1100

    Delete vim chinese docs.

--- modules/vim/action.sh
+++ modules/vim/action.sh
@@ -3,6 +3,7 @@
 source "$(dirname "$0")/../../util.sh"
 
 post_install() {
+  mkdir -p "$HOME/.vim/bundle/"
   [ -r "$HOME/.vim/bundle/vundle" ] || git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
   echo "Installing vim plugins ..."
   vim +BundleInstall +qall
