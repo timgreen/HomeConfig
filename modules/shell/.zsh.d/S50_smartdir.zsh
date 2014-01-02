58355d663f6226e265e7f11c6d064df8a586c32a
Tim Green <iamtimgreen@gmail.com>
2014-01-02 15:07:51 +1100

    Support smart dir functions.

---
+++ modules/shell/.zsh.d/S50_smartdir.zsh
@@ -0,0 +1,38 @@
+declare -a smartdir_functions
+declare -a smartdir_title_only_functions
+
+function do_smartdir {
+  local dir="$1"
+  shift
+
+  for func in ${(@o)*}; do
+    dir=$($func "$dir")
+  done
+
+  echo "$dir"
+}
+
+function smartdir {
+  do_smartdir "$1" ${smartdir_functions[@]}
+}
+
+function smartdir_title {
+  do_smartdir "$1" ${smartdir_functions[@]} ${smartdir_title_only_functions[@]}
+}
+
+function smartdir_99_compress_to_20 {
+  local dir="$1"
+  local odir
+
+  # 自动压缩目录名到小于 20 个字符
+  while (( ${#dir} > 20 )) && [[ $dir != $odir ]]; do
+    odir=$dir
+    dir=$(echo $dir | sed "s:\([~/][^/]\)[^/]\+:\1:")
+  done
+
+  echo "$dir"
+}
+
+
+smartdir_title_only_functions+="smartdir_99_compress_to_20"
+
