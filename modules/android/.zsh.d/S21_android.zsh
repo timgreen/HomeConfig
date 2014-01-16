90b61298fcbe563a09d3ff10deae7baacce4a402
Tim Green <iamtimgreen@gmail.com>
2014-01-16 18:30:08 +1100

    Android module.

--- modules/android/.zsh.d/S21_android.zsh
+++ modules/android/.zsh.d/S21_android.zsh
@@ -1,7 +1,7 @@
-export ANDROID_SDK_ROOT="/opt/android-sdk/"
+export ANDROID_SDK_ROOT="$HOME/dev/android/sdk/"
 export ANDROID_SDK_HOME="$ANDROID_SDK_ROOT"
 
 path+=$ANDROID_SDK_ROOT/platform-tools/
+path+=$ANDROID_SDK_ROOT/tools/
 
 export ANDROID_HOME=$ANDROID_SDK_HOME
-export ANDROID_KEYSTORE=~/workspace/android/devtools/keystore
