f36a30062d83d2d11d7c2d117ea68e647717c24d
Tim Green <iamtimgreen@gmail.com>
2014-03-01 21:50:20 +1100

    Add module g8.

---
+++ modules/g8/action.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source "$(dirname "$0")/../../util.sh"
+
+post_install() {
+  [ -x ~/bin/g8 ] || {
+    curl https://raw.github.com/n8han/conscript/master/setup.sh | sh
+    ~/bin/cs n8han/giter8
+  }
+}
+
+pre_uninstall() {
+  rm -f ~/bin/cs
+  rm -f ~/bin/g8
+}
+
+action_main "$@"
