3f8e58a8136fce62ed16cab4c9558476f9786b35
Tim Green <iamtimgreen@gmail.com>
2015-01-15 21:35:47 +1100

    Upgrade scala to 2.11.5

--- modules/scala/action.sh
+++ modules/scala/action.sh
@@ -4,12 +4,15 @@ source "$(dirname "$0")/../../util.sh"
 
 post_install() {
   SCALA_BASE="$HOME/dev/scala/"
-  [ -x "$SCALA_BASE/current/bin/scala" ] || {
+  SCALA_VERSION=scala-2.11.5
+  SCALA_DOWNLOAD_LINK="http://www.scala-lang.org/files/archive/$SCALA_VERSION.tgz"
+  SCALA_DOWNLOAD_PATH="${SCALA_BASE}/$SCALA_VERSION.tgz"
+
+  [ -x "$SCALA_BASE/current/bin/scala" ] && [ -r "$SCALA_DOWNLOAD_PATH" ] || {
     mkdir -p "$SCALA_BASE"
     cd "$SCALA_BASE"
-    SCALA_VERSION=scala-2.11.0
-    aria2c http://www.scala-lang.org/files/archive/$SCALA_VERSION.tgz
-    tar xf $SCALA_VERSION.tgz
+    aria2c "$SCALA_DOWNLOAD_LINK"
+    tar xf "$SCALA_DOWNLOAD_PATH"
     rm -f current
     ln -s $SCALA_VERSION current
   }
