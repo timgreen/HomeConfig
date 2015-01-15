44d7e20eb2b011d708c1268a521411dcd29ba04c
Tim Green <iamtimgreen@gmail.com>
2015-01-15 16:45:03 +1100

    Hide vundle install screen.

--- modules/vim/action.sh
+++ modules/vim/action.sh
@@ -6,7 +6,7 @@ post_install() {
   mkdir -p "$HOME/.vim/bundle/"
   [ -r "$HOME/.vim/bundle/vundle" ] || git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
   echo "Installing vim plugins ..."
-  vim +BundleInstall +qall
+  vim +BundleInstall +qall &> /dev/null
   tput cuu1
   tput ed
 }
