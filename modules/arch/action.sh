009921a70f6959c7358ace7c253e27a8d4a256ca
Tim Green <iamtimgreen@gmail.com>
2016-01-29 10:14:54 +1100

    [arch] Require `jq`.

--- modules/arch/action.sh
+++ modules/arch/action.sh
@@ -10,6 +10,8 @@ check() {
   require "Only for Arch Linux" check_arch
   require "Need aria2c installed" \
     'type -p aria2c'
+  require "Need jq installed" \
+    'type -p jq'
 }
 
 action_main "$@"
