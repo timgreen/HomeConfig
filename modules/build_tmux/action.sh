a8bcce070cac1af44fb1d5adaa49747ed0bc6a60
Tim Green <iamtimgreen@gmail.com>
2016-02-03 10:50:31 +1100

    [build_tmux] Config download path

--- modules/build_tmux/action.sh
+++ modules/build_tmux/action.sh
@@ -5,16 +5,18 @@ source "$(dirname "$0")/../../util.sh"
 check_tmux_version() {
   # TODO(timgreen): read version from config
   ! type -p tmux || \
-    verlt $(tmux -V | awk '{print $2}') $(get_version)
+    verlt $(tmux -V | awk '{print $2}') $(get version)
 }
 
 check() {
-  require "tmux >= $(get_version) already installed" check_tmux_version
+  require "tmux >= $(get version) already installed" check_tmux_version
 }
 
 post_install() {
   # build & install tmux
-  echo "build & install tmux $(get_version)"
+  echo "build & install tmux $(get version)"
+  url=$(path_for_version $(get url_tpl) $(get version))
+  echo "Downloading $url"
 }
 
 action_main "$@"
