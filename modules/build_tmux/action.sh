26ec217861042f52b8222f453a690da8bf7c34d2
Tim Green <iamtimgreen@gmail.com>
2016-02-02 18:05:53 +1100

    [build_tmux] read version from config.

--- modules/build_tmux/action.sh
+++ modules/build_tmux/action.sh
@@ -5,16 +5,16 @@ source "$(dirname "$0")/../../util.sh"
 check_tmux_version() {
   # TODO(timgreen): read version from config
   ! type -p tmux || \
-    verlt $(tmux -V | awk '{print $2}') 2.1
+    verlt $(tmux -V | awk '{print $2}') $(get_version)
 }
 
 check() {
-  require "tmux >= 2.1 already installed" check_tmux_version
+  require "tmux >= $(get_version) already installed" check_tmux_version
 }
 
 post_install() {
   # build & install tmux
-  echo "build & install tmux"
+  echo "build & install tmux $(get_version)"
 }
 
 action_main "$@"
