bff9e3bf691193fc1affd1892a5bbd52bd69e583
Tim Green <iamtimgreen@gmail.com>
2016-04-27 23:16:48 +1000

    [shell] install zplug.

--- modules/shell/.zsh.d/S99_zplug.zsh
+++ modules/shell/.zsh.d/S99_zplug.zsh
@@ -1,5 +1,6 @@
 source ~/.zplug/zplug
 
+zplug "b4b4r07/zplug"
 zplug "supercrabtree/k"
 # zsh-syntax-highlighting must be loaded after executing compinit command and sourcing other plugins
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
---
+++ modules/shell/action/action.sh
@@ -0,0 +1,3 @@
+pre_install() {
+  [ -d ~/.zplug ] || git clone https://github.com/b4b4r07/zplug ~/.zplug
+}
