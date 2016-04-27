151a696be39520898de9e5d522f706f3cafe65e6
Tim Green <iamtimgreen@gmail.com>
2016-04-27 23:39:09 +1000

    [shell] zplug fzf.

--- modules/shell/.zsh.d/S99_zplug.zsh
+++ modules/shell/.zsh.d/S99_zplug.zsh
@@ -5,4 +5,13 @@ zplug "supercrabtree/k"
 # zsh-syntax-highlighting must be loaded after executing compinit command and sourcing other plugins
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 
+# fzf & fzf-tmux
+zplug "junegunn/fzf-bin", \
+    as:command, \
+    from:gh-r, \
+    rename-to:fzf, \
+    use:"*linux*amd64*"
+zplug "junegunn/fzf", as:command, use:bin/fzf-tmux
+
+# load
 zplug load --verbose
