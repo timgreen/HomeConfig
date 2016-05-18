072956ced3a3d2966f30c7a25caae49ed4e9c966
Tim Green <iamtimgreen@gmail.com>
2016-05-18 16:16:31 +1000

    [vim] switch to use vim-plug.

--- modules/vim/action/action.sh
+++ modules/vim/action/action.sh
@@ -1,12 +1,6 @@
 post_install() {
-  mkdir -p "$HOME/.vim/bundle/"
-  [ -r "$HOME/.vim/bundle/vundle" ] || git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
   echo "Installing vim plugins ..."
-  vim +BundleInstall +qall &> /dev/null
+  vim +PlugInstall +qall &> /dev/null
   tput cuu1
   tput ed
-}
-
-pre_uninstall() {
-  rm -fr "$HOME/.vim/bundle/"
 }
