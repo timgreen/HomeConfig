ea7d2fa15c3949e7dd63e36367268f5e70453fc7
Tim Green <iamtimgreen@gmail.com>
2016-09-16 14:53:18 +1000

    [git] alias for fzf_git.

--- modules/git/action/action.sh
+++ modules/git/action/action.sh
@@ -2,4 +2,7 @@ post_install() {
   git config --global --get include.path \.gitconfig_common &> /dev/null || {
     git config --global --add include.path .gitconfig_common
   }
+  git config --global --get include.path \.gitconfig_fzf &> /dev/null || {
+    git config --global --add include.path .gitconfig_fzf
+  }
 }
