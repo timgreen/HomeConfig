f0cc3f8b8e32e51f18a4326392fc6fc0573a5bb1
Tim Green <iamtimgreen@gmail.com>
2017-04-13 23:09:30 +1000

    Group for git.
    - git
    - tig
    - fzf_git

--- legacy/modules/git/action/action.sh
+++
@@ -1,8 +0,0 @@
-post_install() {
-  git config --global --get include.path \.gitconfig_common &> /dev/null || {
-    git config --global --add include.path .gitconfig_common
-  }
-  git config --global --get include.path \.gitconfig_fzf &> /dev/null || {
-    git config --global --add include.path .gitconfig_fzf
-  }
-}
