ea0aa808e71df374fdfdc66e73a8371788ffa683
Tim Green <iamtimgreen@gmail.com>
2017-05-16 13:29:46 +1000

    Make tmuxifier to support true color.

--- cm_pool/term_multiplexer/tmux/.zsh.d/S21_tmuxifier.zsh
+++ cm_pool/term_multiplexer/tmux/.zsh.d/S21_tmuxifier.zsh
@@ -1,5 +1,4 @@
 export TMUXIFIER="$HOME/.tmux/tmuxifier"
 export TMUXIFIER_LAYOUT_PATH="$HOME/.tmux/tmuxifier-layouts"
-export TMUXIFIER_TMUX_OPTS="-2"
 path+=$TMUXIFIER/bin
 source "$TMUXIFIER/completion/tmuxifier.zsh"
