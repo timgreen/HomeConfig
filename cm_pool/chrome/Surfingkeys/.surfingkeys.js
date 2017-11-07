a7a1bcc7903494206a47387edaed3fb9e6a84934
Tim Green <iamtimgreen@gmail.com>
2017-11-07 16:36:09 +1100

    [chrome] add config for Surfingkeys.

---
+++ cm_pool/chrome/Surfingkeys/.surfingkeys.js
@@ -0,0 +1,14 @@
+unmap('se');
+unmap('sb');
+unmap('ow');
+unmap('B');
+unmap('ga');
+unmap('F');
+
+// Follow links
+settings.hintAlign = 'left';
+Hints.characters = 'asdfghjkl';
+
+// only keep E, R and T from Surfingkeys for some sites.
+unmapAllExcept(['E','R','T'], /mail.google.com|inbox.google.com/);
+
