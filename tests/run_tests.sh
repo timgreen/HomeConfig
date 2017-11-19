950150903f1b1a61de18de062b056da958b7ccce
Tim Green <iamtimgreen@gmail.com>
2017-11-19 23:54:53 +1100

    Test hcm sync.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,5 +2,6 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
+rm -fr test_home 
 mkdir -p test_home
 docker-compose -f docker/docker-compose.yml run --user="$UID:$GID" test_init_sync
--- tests/test_hcm_sync.sh
+++ tests/test_hcm_sync.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-ls 
+cd $HOME
+
+# install hcm
+wget --no-check-certificate https://github.com/timgreen/hcm/archive/v1.zip
+unzip v1.zip
+
+# link hcm.yml
+mkdir -p ~/.hcm
+ln -s /source/hcm.yml ~/.hcm/hcm.yml
+
+# hcm sync
+./hcm-1/hcm sync -f
