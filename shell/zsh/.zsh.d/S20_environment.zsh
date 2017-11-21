fc578e6174d06f7261603a8effd81177a20d40bc
Tim Green <iamtimgreen@gmail.com>
2017-11-22 01:33:03 +1100

    Migrate shell/zsh.

--- shell/zsh/.zsh.d/S20_environment.zsh
+++ shell/zsh/.zsh.d/S20_environment.zsh
@@ -4,11 +4,7 @@ export LC_ALL=en_US.UTF8
 ## Editor
 export EDITOR=vim
 
-## Git
-export GIT_PAGER='less -R'
-
-## Path {{{
-# TODO(timgreen): use wildcard.
+## Path
 path=(
   $HOME/sh/
   $HOME/bin/
@@ -22,11 +18,6 @@ path=(
   /usr/sbin/                                # sbin
   /usr/local/sbin                           # local sbin
 )
-## }}}
 
 # This removes duplicates from PATH, CDPATH, MANPATH, and FPATH
 typeset -U path manpath fpath
-
-
-## ibus
-declare -x XMODIFIERS="@im=ibus"
