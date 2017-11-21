02cd163aab0bf0210279b86bcd9a9a4339942467
Tim Green <iamtimgreen@gmail.com>
2017-11-22 00:51:46 +1100

    Clean up test_home before run another image.

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -2,15 +2,14 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-rm -fr test_home
-mkdir -p test_home
-
 images=(
   arch
   debian
 )
 
 for image in ${images[@]}; do
+  rm -fr test_home
+  mkdir -p test_home
   docker-compose \
     -f docker/docker-compose.yml \
     run --user="$UID:$GID" "$image"
