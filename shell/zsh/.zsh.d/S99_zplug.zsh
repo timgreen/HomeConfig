b218e608bcfd38ee4942ba273e1652ada9b21f3a
Tim Green <iamtimgreen@gmail.com>
2017-11-22 21:06:36 +1100

    [zplug] install plugins in post-install hook.
    Instead of in .zshrc.

--- shell/zsh/.zsh.d/S99_zplug.zsh
+++ shell/zsh/.zsh.d/S99_zplug.zsh
@@ -28,13 +28,13 @@ zplug "zsh-users/zsh-autosuggestions"
 zplug "bric3/nice-exit-code"
 zplug "nviennot/zsh-vim-plugin" # support vi file:123
 
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
+# # Install plugins if there are plugins that have not been installed
+# if ! zplug check --verbose; then
+#     printf "Install? [y/N]: "
+#     if read -q; then
+#         echo; zplug install
+#     fi
+# fi
 
 # Then, source plugins and add commands to $PATH
 zplug load
