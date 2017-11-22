85321f4fcdbed9e17c4ccc1aa77c1f0bf7cbbae0
Tim Green <iamtimgreen@gmail.com>
2017-11-23 00:19:14 +1100

    Let zplug manage zplug.
    Fixed #1.

--- shell/zsh/.zsh.d/S99_zplug.zsh
+++ shell/zsh/.zsh.d/S99_zplug.zsh
@@ -4,11 +4,12 @@ export ZPLUG_HOME="$HOME/.zplug/zplug"
 if [[ ! -d $ZPLUG_HOME ]]; then
   mkdir -p $(dirname $ZPLUG_HOME)
   git clone https://github.com/zplug/zplug $ZPLUG_HOME
-  source $ZPLUG_HOME/init.zsh && zplug update --self
+  source $ZPLUG_HOME/init.zsh
 fi
 
 source $ZPLUG_HOME/init.zsh
 
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 zplug "supercrabtree/k"
 # zsh-syntax-highlighting must be loaded after executing compinit command and sourcing other plugins
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
