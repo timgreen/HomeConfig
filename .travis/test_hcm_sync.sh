aa7f700cd21d60bc2bcea0274eb2688d61bdf3fd
Tim Green <iamtimgreen@gmail.com>
2017-11-22 23:56:48 +1100

    Hide unzip output.

--- .travis/test_hcm_sync.sh
+++ .travis/test_hcm_sync.sh
@@ -8,7 +8,7 @@ ls -al
 # install hcm
 wget --no-check-certificate --continue https://github.com/timgreen/hcm/archive/v1.zip
 rm -fr ./hcm-1
-unzip v1.zip
+unzip v1.zip &> /dev/null
 HCM=./hcm-1/hcm
 
 # link hcm.yml
