ad911a8f127568b1335c894ad05cbe2f909a5e2e
Tim Green <iamtimgreen@gmail.com>
2017-11-29 00:26:59 +1100

    [linuxbrew] Include brew fpath.

--- cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
+++ cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
@@ -1,4 +1,5 @@
 path=("$HOME/.linuxbrew/bin" "$HOME/.linuxbrew/sbin" $path)
+fpath+="$HOME/.linuxbrew/share/zsh/site-functions"
 export MANPATH="$(brew --prefix)/share/man:$MANPATH"
 export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
 export XDG_DATA_DIRS="/home/timgreen/.linuxbrew/share:$XDG_DATA_DIRS"
