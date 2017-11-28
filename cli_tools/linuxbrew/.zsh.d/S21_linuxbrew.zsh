2160ded43620c4a52825ca7e2d28eb18b8957477
Tim Green <iamtimgreen@gmail.com>
2017-11-29 00:17:43 +1100

    [linuxbrew] fix `brew doctor` warning.

--- cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
+++ cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
@@ -1,3 +1,4 @@
-path=("$HOME/.linuxbrew/bin" $path)
+path=("$HOME/.linuxbrew/bin" "$HOME/.linuxbrew/sbin" $path)
 export MANPATH="$(brew --prefix)/share/man:$MANPATH"
 export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
+export XDG_DATA_DIRS="/home/timgreen/.linuxbrew/share:$XDG_DATA_DIRS"
