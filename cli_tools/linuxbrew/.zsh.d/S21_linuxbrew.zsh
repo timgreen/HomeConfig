774f5fe22c3bac6f2fb0ce94c9681b2f27f8580e
Tim Green <iamtimgreen@gmail.com>
2018-02-20 23:06:49 +1100

    [linuxbrew] Install linuxbrew to /home/linuxbrew.
    Fix issue #6.

--- cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
+++ cli_tools/linuxbrew/.zsh.d/S21_linuxbrew.zsh
@@ -1,5 +1,6 @@
-path=("$HOME/.linuxbrew/bin" "$HOME/.linuxbrew/sbin" $path)
-fpath+="$HOME/.linuxbrew/share/zsh/site-functions"
-export MANPATH="$(brew --prefix)/share/man:$MANPATH"
-export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
-export XDG_DATA_DIRS="/home/timgreen/.linuxbrew/share:$XDG_DATA_DIRS"
+export LINUXBREW_HOME='/home/linuxbrew/.linuxbrew'
+path=("$LINUXBREW_HOME/bin" "$LINUXBREW_HOME/sbin" $path)
+fpath+="$LINUXBREW_HOME/share/zsh/site-functions"
+export MANPATH="$LINUXBREW_HOME/share/man:$MANPATH"
+export INFOPATH="$LINUXBREW_HOME/share/info:$INFOPATH"
+export XDG_DATA_DIRS="$LINUXBREW_HOME/share:$XDG_DATA_DIRS"
--- sync.sh
+++ sync.sh
@@ -2,5 +2,11 @@
 
 BASE_DIR="$(dirname "$(readlink -f "$0")")"
 
+[ -d /home/linuxbrew ] || {
+  echo "Prepare /home/linuxbrew for prebuilt binary."
+  sudo mkdir /home/linuxbrew
+  sudo chown $(id -un):$(id -ug) /home/linuxbrew
+}
+
 sh $BASE_DIR/cli_tools/hcm/install_hcm.sh
 ~/.hcm/repo/hcm sync -f
