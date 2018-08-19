9c63b3e7f70684c99915d2afbc646e41e33879ca
Tim Green <iamtimgreen@gmail.com>
2018-08-19 21:25:31 +1000

    [cli_tools/remark] include plugin remark-math.

--- cli_tools/remark/.remarkrc.js
+++ cli_tools/remark/.remarkrc.js
@@ -1,24 +1,27 @@
 module.exports = {
   // Setting for remark-stringify
-  "settings": {
-    "bullet": "*",
-    "fences": true,
-    "incrementListMarker": true,
-    "listItemIndent": "tab",
-    "rule": "-",
-    "ruleRepetition": 80,
-    "ruleSpaces": false,
-    "strong": "*",
-    "emphasis": "_",
-    "stringLength": function(s) {
-      return s.replace(/\[(.+)\]\(.+\)/g, function(a, b) {return b;}).length;
-    }
+  settings: {
+    bullet: '*',
+    fences: true,
+    incrementListMarker: true,
+    listItemIndent: 'tab',
+    rule: '-',
+    ruleRepetition: 80,
+    ruleSpaces: false,
+    strong: '*',
+    emphasis: '_',
+    stringLength: function(s) {
+      return s.replace(/\[(.+)\]\(.+\)/g, function(a, b) {
+        return b;
+      }).length;
+    },
   },
   // Setting for remark-lint
-  "plugins": [
-    "remark-preset-lint-recommended",
-    "remark-frontmatter",
-    ["remark-lint-list-item-indent", "tab-size"],
-    ["remark-lint-maximum-line-length", ["warn", 80]],
+  plugins: [
+    'remark-math',
+    'remark-preset-lint-recommended',
+    'remark-frontmatter',
+    ['remark-lint-list-item-indent', 'tab-size'],
+    ['remark-lint-maximum-line-length', ['warn', 80]],
   ],
 };
