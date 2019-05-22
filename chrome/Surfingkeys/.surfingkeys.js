9bbd328be0529d24476f206947001fde0dd5dcd5
Tim Green <iamtimgreen@gmail.com>
2019-05-22 10:13:22 +1000

    [chrome/Surfingkeys] Disable emoji.

--- chrome/Surfingkeys/.surfingkeys.js
+++ chrome/Surfingkeys/.surfingkeys.js
@@ -5,10 +5,11 @@ unmap('B');
 unmap('ga');
 unmap('F');
 
+settings.startToShowEmoji = 0;
+
 // Follow links
 settings.hintAlign = 'left';
 Hints.characters = 'asdfghjkl';
 
 // only keep E, R and T from Surfingkeys for some sites.
-unmapAllExcept(['E','R','T'], /mail.google.com|inbox.google.com/);
-
+unmapAllExcept(['E', 'R', 'T'], /mail.google.com|inbox.google.com/);
