f3b7d65929bf25be6f099520444a943cc81263af
Tim Green <iamtimgreen@gmail.com>
2019-11-15 23:59:31 +1100

    Disable Surfingkeys in colab.

--- chrome/Surfingkeys/.surfingkeys.js
+++ chrome/Surfingkeys/.surfingkeys.js
@@ -12,4 +12,4 @@ settings.hintAlign = 'left';
 Hints.characters = 'asdfghjkl';
 
 // only keep E, R and T from Surfingkeys for some sites.
-unmapAllExcept(['E', 'R', 'T'], /mail.google.com|inbox.google.com/);
+unmapAllExcept(['E', 'R', 'T'], /mail.google.com|inbox.google.com|research.google.com/);
