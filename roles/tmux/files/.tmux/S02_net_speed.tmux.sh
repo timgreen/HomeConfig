0c6884d9e54c558beeef86b9f9812955c8e13736
Tim Green <iamtimgreen@gmail.com>
2022-07-21 13:23:47 +1000

    [tmux] Fix net speed space.

--- roles/tmux/files/.tmux/S02_net_speed.tmux.sh
+++ roles/tmux/files/.tmux/S02_net_speed.tmux.sh
@@ -6,7 +6,7 @@ current_status_right=$(tmux show-options -gqv status-right)
 tmux set-option -g status-right "$current_status_right #{net_speed}"
 current_status_right_length=$(tmux show-options -gqv status-right-length)
 
-tmux set-option -g @net_speed_format "🡇%9s 🡅%9s"
-# width = 1 + 9 + 1 + 1 + 9 = 21
+tmux set-option -g @net_speed_format "🡇%9s 🡅%9s  "
+# width =  2 + 9 + 1 + 2 + 9 = 22
 
-tmux set-option -g status-right-length $(($current_status_right_length + 21))
+tmux set-option -g status-right-length $(($current_status_right_length + 22))
