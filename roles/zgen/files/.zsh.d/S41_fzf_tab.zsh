b958bfbdd278e02f515a8fe3d9ebce391196d424
Tim Green <iamtimgreen@gmail.com>
2022-10-07 15:18:41 +1100

    [fzf-tab] Apply common config.

--- roles/zgen/files/.zsh.d/S41_fzf_tab.zsh
+++ roles/zgen/files/.zsh.d/S41_fzf_tab.zsh
@@ -1,1 +1,13 @@
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+
+# Common configuration: https://github.com/Aloxaf/fzf-tab#configure
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
